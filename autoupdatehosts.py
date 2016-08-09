904020981c199638e186b0c2e8a8e624c596698c
glados <lixingce@foxmail.com>
2016-08-09 20:18:44 +0800

    review with oop

--- autoupdatehosts.py
+++ autoupdatehosts.py
@@ -1,33 +1,57 @@
 import urllib2
-import platform
 import hashlib
+import platform
+import shutil
 
-orig_file = 'hosts.orig'
-hostsfile = 'hosts'
-searchtext = '255.255.255.255'
-hostaddr = '127.0.1.1'
-hostsurl = 'https://raw.githubusercontent.com/racaljk/hosts/master/hosts'
-
-hostname = platform.node()
-insert_line = hostaddr + '\t' + hostname + '\n'
-try:
-    response = urllib2.urlopen(hostsurl)
-except urllib2.URLError, e:
-    print e.reason
-contents = response.read()
-if not hashlib.md5(contents).hexdigest() == hashlib.md5(open(orig_file, 'rb').read()).hexdigest():
-    insertloaction = contents.index(searchtext)
-    contents = contents[:insertloaction] + insert_line + contents[insertloaction:]
-    with open(hostsfile, 'w') as f:
-        f.write(contents)
-    print 'Done!'
-    try:
-        response = urllib2.urlopen(hostsurl)
-    except urllib2.URLError,e:
-        print e.reason
-    contents = response.read()
-    with open(orig_file, 'w') as f:
-        f.write(contents)
-    print 'Update!'
-else:
-    print 'No Need!'
+class Hosts():
+    hostOS = platform.system()
+    hostsurl = 'https://raw.githubusercontent.com/racaljk/hosts/master/hosts'
+    
+    def __init__(self):
+        if self.hostOS == 'Windows':
+            self.flag = False
+            self.hostsBak = ''
+            self.hostsOrig = ''
+        elif self.hostOS == 'Linux':
+            self.flag = True
+            self.hostsBak = 'hosts.orig'
+            self.hostsOrig = 'hosts'
+            self.searchtext = '255.255.255.255'
+            self.hostname = platform.node()
+            self.hostaddr = '127.0.1.1'
+            self.insertLine = self.hostaddr + '\t' + self.hostname + '\n'
+        try:
+            response = urllib2.urlopen(self.hostsurl)
+        except urllib2.URLError, e:
+            print e.reason
+        self.contents = response.read()
+    
+    def updateHosts(self):
+        with open(self.hostsBak, 'w') as f:
+            f.write(self.contents)
+        shutil.copy(self.hostsBak, self.hostsOrig)
+        if self.flag:
+            with open(self.hostsOrig, 'r+') as f:
+                content = f.read()
+                i = content.index(self.searchtext)
+                content = content[:i] + self.insertLine + content[i:]
+                f.seek(0)
+                f.write(content)
+                f.truncate()
+        print 'Done'
+    
+    def checkHostsHash(self):
+        if self.flag:
+            whichHosts = self.hostsBak
+        else:
+            whichHosts = self.hostsOrig
+        if hashlib.md5(self.contents).hexdigest() == hashlib.md5(open(whichHosts, 'rb').read()).hexdigest():
+            return True
+        else:
+            return False
+def auto():
+    hostsinstance = Hosts()
+    if not hostsinstance.checkHostsHash():
+        hostsinstance.updateHosts()
+    else:
+        print 'No Need'
